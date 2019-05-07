4b62581d19899c3ed3b44c6ee50c9d5c857d87b4
socurites <socurites@wise-fashion.com>
2019-05-07 23:08:11 +0900

    Add activation function example

---
+++ chap3/activation_function.py
@@ -0,0 +1,60 @@
+"""Step function
+"""
+import numpy as np
+
+
+def step_function(x):
+    y = x > 0
+    return y.astype(np.int)
+
+
+x = np.array([-1.0, 1.0, 2.0])
+y = step_function(x)
+print(y)
+
+# Plot
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+
+import matplotlib.pyplot as plt
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+
+"""Sigmoid function
+"""
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+x = np.array([-1.0, 1.0, 2.0])
+y = sigmoid(x)
+print(y)
+
+# Plot
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+
+import matplotlib.pyplot as plt
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+
+"""ReLU fuction
+"""
+def relu(x):
+    return np.maximum(0, x)
+
+x = np.array([-1.0, 1.0, 2.0])
+y = relu(x)
+print(y)
+
+# Plot
+x = np.arange(-5.0, 5.0, 0.1)
+y = relu(x)
+
+import matplotlib.pyplot as plt
+plt.plot(x, y)
+plt.ylim(-0.1, 5.1)
+plt.show()
