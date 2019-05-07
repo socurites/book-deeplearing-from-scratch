ac3170a9410a00d5d402bd39c0540fd6776936c0
socurites <socurites@wise-fashion.com>
2019-05-07 22:39:52 +0900

    Add perceptron example

---
+++ chap2/perceptron.py
@@ -0,0 +1,91 @@
+def AND1(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+
+    tmp = x1*w1 + x2*w2
+
+    if tmp <= theta:
+        return 0
+    else:
+        return 1
+
+print("AND1")
+print(AND1(0, 0))
+print(AND1(1, 0))
+print(AND1(0, 1))
+print(AND1(1, 1))
+
+
+import numpy as np
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+
+    tmp = np.sum(w*x) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+print("AND")
+print(AND(0, 0))
+print(AND(1, 0))
+print(AND(0, 1))
+print(AND(1, 1))
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+
+    tmp = np.sum(w*x) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+print("NAND")
+print(NAND(0, 0))
+print(NAND(1, 0))
+print(NAND(0, 1))
+print(NAND(1, 1))
+
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+
+    tmp = np.sum(w*x) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+print("OR")
+print(OR(0, 0))
+print(OR(1, 0))
+print(OR(0, 1))
+print(OR(1, 1))
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+
+    return y
+
+
+print("OR")
+print(XOR(0, 0))
+print(XOR(1, 0))
+print(XOR(0, 1))
+print(XOR (1, 1))
+
