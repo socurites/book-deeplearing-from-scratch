913ddca8e3f6854f08fe19ab006b13f6b16bd2d0
Junyi Song <socurites@Junyis-MacBook-Pro.local>
2019-05-15 18:16:07 +0900

    Add mnist train example

--- chap4/gd_train_mnist_train.py
+++ chap4/gd_train_mnist_train.py
@@ -10,3 +10,21 @@ train_loss_list = []
 # Hyper-parameters
 iters_num = 10000
 train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+    print("{}th iteration loss: {}".format(i, loss))
+
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
