5b3d0b0a7bc398801fa35c14539a4d340fefbf93
socurites <socurites@gmail.com>
2019-05-17 20:15:45 +0900

    Add neural layer example

---
+++ chap5/nn_layer/softmax_with_loss.py
@@ -0,0 +1,22 @@
+from chap3.out_fuction import softmax
+from  chap4.loss_function import cross_entropy
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
